43ded2ddaed5d6c1c7ec67763e1eb93992267f73
DT3264 <cernadaniel32@gmail.com>
2022-05-12 00:27:33 -0500

    Added UI components and native capacitor examples

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,8 +1,43 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCardContent,
+  IonItem,
+  IonLabel,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonToggle,
+  IonCheckbox,
+  IonList,
+  useIonAlert,
+  IonItemDivider,
+  IonInput,
+  IonPopover,
+} from "@ionic/react";
+import React, { useState } from "react";
+import { add } from "ionicons/icons";
+
+import "./Tab1.css";
+
+const checkboxList = [
+  { val: "Pepperoni", isChecked: true },
+  { val: "Sausage", isChecked: false },
+];
 
 const Tab1: React.FC = () => {
+  const [present] = useIonAlert();
+  const [checked, setChecked] = useState(false);
+  const [toggleChecked, setToggleChecked] = useState(false);
+  const [text, setText] = useState<string>();
   return (
     <IonPage>
       <IonHeader>
@@ -11,12 +46,68 @@ const Tab1: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonLabel>Alertas</IonLabel>
+        <IonButton
+          expand="block"
+          onClick={() => present("hello with params", [{ text: "Ok" }])}
+        >
+          Mostrar alerta
+        </IonButton>
+        <IonItemDivider />
+        <IonLabel>Tarjetas</IonLabel>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>Card Title</IonCardTitle>
+            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+          </IonCardHeader>
+          <IonCardContent>
+            Keep close to Nature's heart... and break clear away, once in
+            awhile, and climb a mountain or spend a week in the woods. Wash your
+            spirit clean.
+          </IonCardContent>
+        </IonCard>
+        <IonItemDivider />
+        <IonLabel>Listas y checkboxes</IonLabel>
+        <IonList>
+          {checkboxList.map(({ val, isChecked }, i) => (
+            <IonItem key={i}>
+              <IonLabel>{val}</IonLabel>
+              <IonCheckbox slot="end" value={val} checked={isChecked} />
+            </IonItem>
+          ))}
+        </IonList>
+        <IonItemDivider />
+        <IonLabel>Switch</IonLabel>
+        <IonItem>
+          <IonLabel>Activo: {JSON.stringify(toggleChecked)}</IonLabel>
+          <IonToggle
+            checked={toggleChecked}
+            onIonChange={(e) => setToggleChecked(e.detail.checked)}
+          />
+        </IonItem>
+        <IonItemDivider />
+        <IonLabel>Entrada con placeholder</IonLabel>
+        <IonItem>
+          <IonInput
+            value={text}
+            placeholder="Ingrese algo"
+            onIonChange={(e) => setText(e.detail.value!)}
+          ></IonInput>
+        </IonItem>
+        <IonItemDivider />
+        <IonLabel>Dialogos</IonLabel>
+        <br />
+        <IonButton id="side-button">Click para abrir el dialogo</IonButton>
+        <IonPopover trigger="side-button" side="top">
+          <IonContent>Popover Content</IonContent>
+        </IonPopover>
+        {/* <IonLabel>Boton flotante</IonLabel> */}
+        <IonItemDivider />
+        <IonFab vertical="bottom" horizontal="end" slot="fixed">
+          <IonFabButton>
+            <IonIcon icon={add} />
+          </IonFabButton>
+        </IonFab>
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,6 +1,68 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonRow,
+  IonCol,
+} from "@ionic/react";
+import "./Tab2.css";
+
+import { Camera, CameraResultType, CameraSource } from "@capacitor/camera";
+import { Toast } from "@capacitor/toast";
+import { Clipboard } from "@capacitor/clipboard";
+import { Dialog } from "@capacitor/dialog";
+import { Device } from "@capacitor/device";
+import { Haptics, ImpactStyle } from "@capacitor/haptics";
+
+const takePicture = async () => {
+  await Camera.getPhoto({
+    resultType: CameraResultType.Uri,
+    source: CameraSource.Camera,
+    quality: 100,
+  });
+};
+
+const showHelloToast = async () => {
+  await Toast.show({
+    text: "Hello!",
+  });
+};
+
+const writeToClipboard = async () => {
+  await Clipboard.write({
+    string: "Hello World!",
+  });
+};
+
+const checkClipboard = async () => {
+  const { type, value } = await Clipboard.read();
+  console.log();
+  await Toast.show({
+    text: `Got ${type} from clipboard: ${value}`,
+  });
+};
+
+const showAlert = async () => {
+  await Dialog.alert({
+    title: "Stop",
+    message: "this is an error",
+  });
+};
+
+const logDeviceInfo = async () => {
+  const info = await Device.getInfo();
+  console.log(info);
+  await Toast.show({
+    text: `Marca: ${info.manufacturer}, Modelo: ${info.model}, Version: ${info.osVersion}`,
+  });
+};
+
+const hapticsImpactMedium = async () => {
+  await Haptics.impact({ style: ImpactStyle.Heavy });
+};
 
 const Tab2: React.FC = () => {
   return (
@@ -11,12 +73,33 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <IonButton expand="block" onClick={() => takePicture()}>
+          Camara
+        </IonButton>
+        <IonButton expand="block" onClick={() => showHelloToast()}>
+          Toast
+        </IonButton>
+        <IonRow>
+          <IonCol size="6">
+            <IonButton expand="block" onClick={() => writeToClipboard()}>
+              Guardar datos en el portapapeles
+            </IonButton>
+          </IonCol>
+          <IonCol size="6">
+            <IonButton expand="block" onClick={() => checkClipboard()}>
+              Leer datos del portapapeles
+            </IonButton>
+          </IonCol>
+        </IonRow>
+        <IonButton expand="block" onClick={() => showAlert()}>
+          Mostrar alerta
+        </IonButton>
+        <IonButton expand="block" onClick={() => logDeviceInfo()}>
+          Informacion del dispositivo
+        </IonButton>
+        <IonButton expand="block" onClick={() => hapticsImpactMedium()}>
+          Vibración media
+        </IonButton>
       </IonContent>
     </IonPage>
   );
